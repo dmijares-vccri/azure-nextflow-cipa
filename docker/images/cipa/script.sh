13879ea5d49b1768495c0f63ee2b6ed8bd10fb01
dmijares-vccrit <d.mijares@victorchang.edu.au>
2023-02-17 12:39:01 +1100

    separate logs by sample

--- docker/images/cipa/script.sh
+++ docker/images/cipa/script.sh
@@ -7,7 +7,7 @@
 ###
 # Constants
 LOG_DIRECTORY="${APP_DIR}/cipa/hERG_fitting/logs"
-DEFAULT_SAMPLES="0" #up to 2000
+DEFAULT_SAMPLES=0 #up to 2000
 DEFAULT_ACCURACY=0.001
 DEFAULT_POPULATION=80
 
@@ -19,8 +19,8 @@ population=${4:-$DEFAULT_POPULATION}
 accuracy=${5:-$DEFAULT_ACCURACY}
 
 # Define log file
-timestamp=$(date +%Y%m%d_%H%M%S)
-log_name=${drug}_${timestamp}_${samples}.log
+timestamp=$(date +%y%m%d_%H%M%S)
+log_name=${drug}_${timestamp}
 log_file="${LOG_DIRECTORY}/${log_name}"
 
 # Input validation
@@ -30,8 +30,8 @@ exit 1
 fi
 
 # Run the first Rscript
-if ! Rscript generate_bootstrap_samples.R -d "$drug" > "$log_file"; then
-echo "Error: generate_bootstrap_samples.R failed. Check log file for details: $log_name"
+if ! Rscript generate_bootstrap_samples.R -d "$drug" > "${log_file}_generate_bootstrap_samples.R.txt"; then
+echo "Error: generate_bootstrap_samples.R failed. Check log file for details: ${log_file}_generate_bootstrap_samples.R.txt"
 exit 1
 fi
 
@@ -39,14 +39,10 @@ fi
 # Start a for loop that will repeat a set number of times
 for ((counter=0; counter<=$samples; counter++))
 do
-  if ! Rscript hERG_fitting.R -d "$drug" -c "$cpu" -i "$counter" -l "$population" -t "$accuracy" >> "$log_file"; then
-    echo "Error: hERG_fitting.R failed. Check log file for details: $log_name"
+  if ! Rscript hERG_fitting.R -d "$drug" -c "$cpu" -i "$counter" -l "$population" -t "$accuracy" > "${log_file}_sample_${counter}.txt"; then
+    echo "Error: hERG_fitting.R failed. Check log file for details: ${log_file}_sample_${counter}.txt"
     exit 1
   fi
 done
-# if ! Rscript hERG_fitting.R -d "$drug" -c "$cpu" -i "$samples" -l "$population" -t "$accuracy" >> "$log_file"; then
-# echo "Error: hERG_fitting.R failed. Check log file for details: $log_name"
-# exit 1
-# fi
 
-echo "Success: Rscripts completed successfully. Log file: $log_name"
+echo "Success: Rscripts completed successfully. Log file: $log_file"
